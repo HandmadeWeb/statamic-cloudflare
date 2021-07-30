21a670d22fa2c1aa052d1d10512015e928838005
Michael Rook <michael@rook.net.au>
2021-07-30 16:28:24 +1000

    Add notes about isConfigured.

--- src/Traits/RegistersCacher.php
+++ src/Traits/RegistersCacher.php
@@ -8,6 +8,9 @@ trait RegistersCacher
 {
     public static function registerCacher()
     {
+        // Only register the cloudflare static cache drive, if the Api has been configured.
+        // If it has not, then register NullCacher, so that the application doesn't have a tantrum,
+        // if someone is using the cloudflare driver in their static cache.
         if (static::isConfigured()) {
             app(\Statamic\StaticCaching\StaticCacheManager::class)->extend('cloudflare', function ($app, $config, $name) {
                 return new \HandmadeWeb\StatamicCloudflare\Cachers\CloudflareCacher($app[\Illuminate\Cache\Repository::class], $config);
