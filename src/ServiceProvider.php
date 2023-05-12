dd37743544e9fade12ecf6102d0d17c0a4c5dd5e
Jim Blue <10623367+jimblue@users.noreply.github.com>
2023-05-12 20:32:38 +0200

    Support Statamic V4

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,11 +3,8 @@
 namespace HandmadeWeb\StatamicCloudflare;
 
 use HandmadeWeb\StatamicCloudflare\Http\Controllers\CloudflareUtilityController;
-use Illuminate\Cache\Repository;
 use Statamic\Facades\Utility;
 use Statamic\Providers\AddonServiceProvider;
-use Statamic\Statamic;
-use Statamic\StaticCaching\StaticCacheManager;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -30,7 +27,7 @@ class ServiceProvider extends AddonServiceProvider
         ],
     ];
 
-    public function boot()
+    public function bootAddon()
     {
         $static_cacher = config('statamic.static_caching.strategy');
 
@@ -56,17 +53,18 @@ class ServiceProvider extends AddonServiceProvider
 
         // Enable functionality if Cloudflare Api has been configured.
         if (Cloudflare::isConfigured()) {
-            Utility::make('cloudflare')
-            ->action([CloudflareUtilityController::class, 'index'])
-            ->title(__('Cloudflare Manager'))
-            ->icon('earth')
-            ->navTitle(__('Cloudflare'))
-            ->description('Purge Cloudflare from the comfort of your Statamic CP.')
-            // ->docsUrl(Statamic::docsUrl('utilities/cache-manager'))
-            ->routes(function ($router) {
-                $router->post('/purgeAll', [CloudflareUtilityController::class, 'purgeAll'])->name('purgeAll');
-            })
-            ->register();
+            Utility::extend(function () {
+                $utility = Utility::register('cloudflare')
+                    ->title(__('Cloudflare Manager'))
+                    ->navTitle(__('Cloudflare'))
+                    ->description('Purge Cloudflare from the comfort of your Statamic CP.')
+                    ->icon('earth')
+                    ->action([CloudflareUtilityController::class, 'index']);
+
+                $utility->routes(function ($router) {
+                    $router->post('/purgeAll', [CloudflareUtilityController::class, 'purgeAll'])->name('purgeAll');
+                });
+            });
         }
     }
 }
